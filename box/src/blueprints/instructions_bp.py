1252e8c9e0dbd6b68a53e3531864496ccfec7e0f
theowiik <42991003+theowiik@users.noreply.github.com>
2024-05-12 23:56:25 +0200

    Add pydantic models for instructins

--- box/src/blueprints/instructions_bp.py
+++ box/src/blueprints/instructions_bp.py
@@ -1,14 +1,46 @@
-from flask import Blueprint, jsonify
-from webargs import fields
-from webargs.flaskparser import use_args
+from flask import Blueprint, jsonify, request
+from pydantic import BaseModel, ValidationError
+from typing import List, Optional
 
 instructions_bp = Blueprint("instructions", __name__)
-instructions_dto = {}
 
 
 @instructions_bp.route("/instructions", methods=["POST"])
-@use_args(instructions_dto, location="json")
-def run_instructions(args: dict):
-    print("doing some instructions")
+def run_instructions():
+    try:
+        data = request.get_json()
+        instructions_set = InstructionsSet(**data)
+        print("Parsed data:", instructions_set.json(indent=2))
+    except ValidationError as e:
+        return jsonify({"error": str(e)}), 400
+
+    print("Doing some instructions")
 
     return jsonify({"status": "okidoki"}), 200
+
+
+class LightInstruction(BaseModel):
+    enabled: bool
+    color: str
+
+
+class SoundInstruction(BaseModel):
+    name: str
+
+
+class Step(BaseModel):
+    action: str
+    light: Optional[LightInstruction]
+    sound: Optional[SoundInstruction]
+    blocking: bool
+    duration_ms: int
+
+
+class AudioFile(BaseModel):
+    name: str
+    data: str
+
+
+class InstructionsSet(BaseModel):
+    audio_files: List[AudioFile]
+    instructions: List[Step]
